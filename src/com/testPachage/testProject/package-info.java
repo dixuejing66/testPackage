0a2043112f3993e121759505c10fc40cad3f5179
dixuejing66 <dixuejing66@gmail.com>
2020-09-28 17:06:17 +0900

    dadf

---
+++ src/com/testPachage/testProject/package-info.java
@@ -0,0 +1,1 @@
+package com.testPachage.testProject;
--- src/com/testPackage/testProject/Test.java
+++
@@ -1,8 +0,0 @@
-package com.testPackage.testProject;
-
-public class Test {
-
-	public static void main(String[] args) {
-		System.out.println("Hello!");
-	}
-}
